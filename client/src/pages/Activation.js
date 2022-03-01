75d0f3e1239c7711976c116c14f78d037b2b9f45
Vivi Tzekou <vivi1704@gmail.com>
2022-03-01 10:28:27 +0200

    New css

--- client/src/pages/Activation.js
+++ client/src/pages/Activation.js
@@ -6,11 +6,28 @@ import activation02 from '../images/activation-02.png';
 import activation03 from '../images/activation-03.png';
 import activation04 from '../images/activation-04.png';
 
+import entinf1 from '../pages/img/Image-enter-your-informations.png';
+import entinf2 from '../pages/img/Image-enter-your-informations@2x.png';
+import entinf3 from '../pages/img/Image-enter-your-informations@3x.png';
+
+import entcode1 from '../pages/img/Image-enter-your-code.png';
+import entcode2 from '../pages/img/Image-enter-your-code@2x.png';
+import entcode3 from '../pages/img/Image-enter-your-code@3x.png';
+
+import scan1 from '../pages/img/image-scan-with-your-mobile-wallet.png';
+import scan2 from '../pages/img/image-scan-with-your-mobile-wallet@2x.png';
+import scan3 from '../pages/img/image-scan-with-your-mobile-wallet@3x.png';
+
+
+import cert1 from '../pages/img/Image-accept-your-certificate.png';
+import cert2 from '../pages/img/Image-accept-your-certificate@2x.png';
+import cert3 from '../pages/img/Image-accept-your-certificate@3x.png';
+
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
-    Container, Button, Row, Col, Image
-  } from 'react-bootstrap';
+  Container, Button, Row, Col, Image
+} from 'react-bootstrap';
 import { Link } from "react-router-dom";
 import UseCases from "../components/UseCases";
 import ManageStrip from "../components/ManageStrip";
@@ -21,143 +38,148 @@ function Activation() {
   const [data, setData] = React.useState(null);
 
   React.useEffect(() => {
-    fetch(REACT_APP_NODE_LOCAL+"/api")
-      .then((res) =>  res.json())
+    fetch(REACT_APP_NODE_LOCAL + "/api")
+      .then((res) => res.json())
       .then((data) => setData(data.message));
   }, []);
-  
+
   return (
     <div className="Activation">
       <main className="my-5 py-5">
-        <Container className="px-0">
-            <div className="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
-            <div className="col-md-10 p-lg-10 mx-auto my-10">
-                <Row><h1 className="display-4 fw-normal">Activate your pass</h1></Row>
+        <Container>
+          <Row className="position-relative p-3 p-md-5 m-md-3 bg-playground text-white  align-content-center">
+            <Col xs={12} md={12} lg={{ span: 6, offset: 3 }} className="justify-content-center">
+
               <Row>
-              <p className="lead fw-normal">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+                <Col>
+                <span className="Title-TAGH1 text-center p-3">Activate your pass</span>
+                </Col>
               </Row>
               <Row>
-                  <Col>
-                <Button variant="dark">Start Demo</Button>
-              </Col>
+                <Col>
+                <span className="Title-TAGH3 text-center p-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</span>
+                </Col>
               </Row>
-              <Row style={{padding:20}}>
-                  <Col>
-                    <Button variant="outline-dark" href="#description">How does it work?</Button>
+              <Row className='p-5'>
+                <Col>
+                  <Button variant="btn btn-info btn-lg ">Start Demo</Button>
+                </Col>
+              </Row>
+              <Row className="p-3">
+                <Col>
+                  <Button variant="outline-light " href="#description">How does it work?</Button>
                 </Col>
               </Row>
 
-            </div>
-            
-          </div>
+            </Col>
+          </Row>
+
 
           <div id="description" className="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-white">
-                <div className="col-md-10 p-lg-10 mx-auto my-10">
-                    <Row><h1 className="display-4 fw-normal">From your desktop</h1></Row>
-                <Row>
-                    <Col xs md lg= {{ span: 6, offset: 3 }}>
-                        <hr style={{backgroundColor:"black",height:"2px"}}/>
-                    </Col>
+
+            <Row><Col><span className="Title-TAGH1-dark text-center p-3">From your desktop</span></Col></Row>
+
+
+            <Row className="position-relative overflow-hidden p-2 p-md-4 m-md-2">
+              <Col xs={12} md={12} lg={{ span: 6 }}>
+                <Row >
+                  <Col className="text-end" xs={3} md={3} lg={{ span: 3 }}>
+                    <h1 className="text-muted">1.</h1>
+                  </Col>
+                  <Col className="text-start">
+                    <span className="Title-TAGH2-dark"> Enter your information</span>
+                  </Col>
                 </Row>
-                
-                </div>
-              
-              <Row className="position-relative overflow-hidden p-2 p-md-4 m-md-2">
-                  <Col xs md lg={{span:6}}>
-                      <Row >
-                          <Col className="text-end" xs md lg={{span:3}}>
-                            <h1 className="text-muted">1.</h1>
-                          </Col>
-                          <Col className="text-start">
-                                <h2> Enter your information</h2>
-                          </Col>
-                      </Row>
-                      <Row>
-                          <Col >
-                            <Image src={activation01} fluid></Image>
-                          </Col>
-                      </Row>
-                      <Row>
-                          <Col >
-                            <p className="text-muted fs-6"><b>1.</b> First name <b>2.</b> Last name <b>3.</b> E-mail</p>
-                          </Col>
-                      </Row>
+                <Row>
+                  <Col >
+                    <Image src={entinf1} srcSet={`${entinf2} 2x, ${entinf3} 3x`} className="step-image-desktop " fluid></Image>
+
                   </Col>
-                  <Col xs md lg={{span:6}}>
-                    <Row>
-                          <Col className="text-end" xs md lg={{span:3}}>
-                            <h1 className="text-muted">2.</h1>
-                          </Col>
-                          <Col className="text-start">
-                                <h2> Enter your code</h2>
-                          </Col>
-                      </Row>
-                      <Row>
-                          <Col className="p-lg-10">
-                            <Image src={activation02} fluid></Image>
-                          </Col>
-                      </Row>
-                      <Row>
-                          <Col >
-                            <p className="text-muted fs-6"><b>1.</b> Enter the code you received by email </p>
-                          </Col>
-                      </Row>
+                </Row>
+                <Row>
+                  <Col >
+                    <p className="text-muted fs-6"><b>1.</b> First name <b>2.</b> Last name <b>3.</b> E-mail</p>
                   </Col>
-              </Row>
-              <Row className="position-relative overflow-hidden p-2 p-md-4 m-md-2">
-                  <Col xs md lg={{span:6}}>
-                      <Row >
-                            <Col className="text-end" xs md lg={{span:3}}>
-                                <h1 className="text-muted">3.</h1>
-                            </Col>
-                            <Col className="text-start">
-                                    <h2> Scan with your mobile wallet</h2>
-                            </Col>
-                          
-                      </Row>
-                      <Row>
-                          <Col >
-                            <Image src={activation03} fluid></Image>
-                          </Col>
-                      </Row>
+                </Row>
+              </Col>
+              <Col xs={12} md={12} lg={{ span: 6 }}>
+                <Row>
+                  <Col className="text-end" xs={3} md={3} lg={{ span: 3 }}>
+                    <h1 className="text-muted">2.</h1>
                   </Col>
-                  <Col xs md lg={{span:6}}>
-                    <Row>
-                            <Col className="text-end" xs md lg={{span:3}}>
-                                <h1 className="text-muted">4.</h1>
-                            </Col>
-                            <Col className="text-start">
-                                    <h2> Accept your certificate</h2>
-                            </Col>
-                         
-                      </Row>
-                      <Row>
-                          <Col className="p-lg-10">
-                            <Image src={activation04} fluid></Image>
-                          </Col>
-                      </Row>
+                  <Col className="text-start">
+
+                    <span className="Title-TAGH2-dark"> Enter your code</span>
                   </Col>
-              </Row>
+                </Row>
+                <Row>
+                  <Col className="p-lg-10">
+                    <Image src={entcode1} srcSet={`${entcode2} 2x, ${entcode3} 3x`} className="step-image-desktop " fluid></Image>
+
+                  </Col>
+                </Row>
+                <Row>
+                  <Col >
+                    <p className="text-muted fs-6"><b>1.</b> Enter the code you received by email </p>
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
+            <Row className="position-relative overflow-hidden p-2 p-md-4 m-md-2">
+              <Col xs={12} md={12} lg={{ span: 6 }}>
+                <Row >
+                  <Col className="text-end" xs={3} md={3} lg={{ span: 3 }}>
+                    <h1 className="text-muted">3.</h1>
+                  </Col>
+                  <Col className="text-start">
+                    <span className="Title-TAGH2-dark"> Scan with your mobile wallet</span>
+                  </Col>
+
+                </Row>
+                <Row>
+                  <Col >
+                    <Image src={scan1} srcSet={`${scan2} 2x, ${scan3} 3x`} className="step-image-mobile" fluid></Image>
+                  </Col>
+                </Row>
+              </Col>
+              <Col xs={12} md={12} lg={{ span: 6 }}>
+                <Row>
+                  <Col className="text-end" xs={3} md={3} lg={{ span: 3 }}>
+                    <h1 className="text-muted">4.</h1>
+                  </Col>
+                  <Col className="text-start">
+                    <span className="Title-TAGH2-dark"> Accept your certificate</span>
+                  </Col>
+
+                </Row>
+                <Row>
+                  <Col className="p-lg-10">
+                    <Image src={cert1} srcSet={`${cert2} 2x, ${cert3} 3x`} className="step-image-mobile" fluid></Image>
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
+          </div>
+
+          <div className="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center ">
+          <Row>
+            <Col xs={12} md={12} lg={{ span: 6, offset: 3 }}>
+              <Button variant="btn btn-info btn-lg btn-demo">Start Demo</Button>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12} md={12} lg={{ span: 10, offset: 1 }}>
+              <hr className="btn-info btn-demo-hr" />
+            </Col>
+          </Row>
           </div>
 
 
-            <Row>
-                <Col xs md lg={{span:6,offset:3}}>
-                    <Button variant="dark">Start Demo</Button>
-                </Col>
-            </Row>
-            <Row>
-                <Col xs md lg={{span:10,offset:1}}>
-                    <hr/>
-                </Col>
-            </Row>
-
-
-          <UseCases/>
-          <ManageStrip/>
+          <UseCases />
+          <ManageStrip />
 
           <p>{!data ? "Loading..." : data}</p>
-        
+
         </Container>
       </main>
     </div>
