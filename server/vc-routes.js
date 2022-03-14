1f378dcf0794bad349489d5fe7f84b0ebcfcb5f9
Vivi Tzekou <vivi1704@gmail.com>
2022-03-14 11:16:16 +0200

    allow list of origins

--- server/vc-routes.js
+++ server/vc-routes.js
@@ -14,8 +14,19 @@ var router = express.Router();
 const upload = multer()
 router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
+var allowedOrigins = ['http://localhost:3000','https://playground.talao.co/'];
 router.use(cors({
-    origin: ['http://localhost:3000','https://playground.talao.co/']
+    origin: function(origin, callback){
+      // allow requests with no origin 
+      // (like mobile apps or curl requests)
+      if(!origin) return callback(null, true);
+      if(allowedOrigins.indexOf(origin) === -1){
+        var msg = 'The CORS policy for this site does not ' +
+                  'allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    }
   }));
 
 
