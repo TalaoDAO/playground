5bfcbe79a5b10342fbf4926d25f68d00ebe2d3d5
superlance <superlance@outlook.com>
2022-05-06 15:40:18 +0100

    feat: update spacings on demo pages

--- client/src/pages/DiscountDemo/index.js
+++ client/src/pages/DiscountDemo/index.js
@@ -43,138 +43,139 @@ function DiscountDemo() {
 
   return (
     <main >
-      <Container className="shadow px-0 ">
-        <Row className="px-0">
-          <Col xs={12} md={12} lg={12}>
-            <Image src={news_header} className="img-fit" fluid></Image>
-
-          </Col>
-
-        </Row>
-
-        <div className="discount-box ">
-
-
-
-          <Row className="text-center">
-            <Col xs={12} md={12} lg={{span:7}}>
-              <Row>
-                <Col>
-                  <div className="discount-title">Passport for a decentralized world</div>
-                </Col>
-              </Row>
-              <Row className="text-left">
-                <div className="discount-info">
-                  {new Date().toLocaleDateString()}
-                  &nbsp;
-                  &nbsp;
-                  &nbsp;
-                  <Image src={comment_icon} className="discount-sub-icons" fluid></Image>
-                  &nbsp;
-                  01
-                  &nbsp;
-                  &nbsp;
-                  &nbsp;
-                  <Image src={eye_icon} className="discount-sub-icons" fluid></Image>
-                  &nbsp;
-                  301
-                </div>
-
-              </Row>
-              <Row className="discount-separation-top">
-                <Col>
-                  <Image src={art01} srcSet={`${art02} 2x, ${art03} 3x`} className="discount-article-img" fluid></Image>
-
-
-                </Col>
-
-              </Row>
-              <Row className="discount-separation-top">
-                <Col className="discount-article">
-
-                  <p>
-                    The decentralized world of Web3 and open Metaverse is upsetting the relationship between platforms and their users. Platforms lose control of data and users are only recognized by their address on the local blockchain.
-
-                  </p>
-                  <p>
-                    The sovereign identity is then absent from the exchanges and it is the community which takes over, the community of a game or the Twitter community of the artist which proves the authenticity of the work linked to the NFT.
-
-                  </p>
-                  <p>
-
-                    But what happens if you leave your community, what happens if the tweet is deleted?
-
-                  </p>
-
-                  <p>
-                    For data it’s even worse, some try to put them in NFTs but not everything is moneyable and transferable. A certificate of professional experience or a loyalty card, whether attached to a user or to his avatar in a Metaverse, has nothing to do with a token.
-
-                  </p>
-
-                  <p>
-                    There is therefore a lack, an approach that allows us to reconstitute a support for what we need to keep with us in this new decentralized world.
-
-                  </p>
-
-                </Col>
-
-              </Row>
-            </Col>
-            <Col xs={12} md={12} lg={{span:4,offset:1}}>
-              <Row className="text-center">
-                <Col>
-                  <Row className="text-center">
-                    <Col>
-                      <div className="discount-perc">15%</div>
-                    </Col>
-                  </Row>
-                  <Row >
-                    <Col className="text-center">
-                      <div className="discount-perc-sub">Discount</div>
-                    </Col>
-                  </Row>
-
-                </Col>
-                <Col className="text-center">
-                  <div className="lg-only">
-                    <QRCode value={REACT_APP_QR_URL + "/discount-offer"+qr} size={128} />
-                  </div>
-                  <div className="sm-only">
-                    <Button className="btn-dark-submit " variant="primary" href={REACT_APP_WALLET_LINK + encodeURIComponent(REACT_APP_QR_URL + "/discount-offer" + qr)}>Click to add to wallet</Button>
-                  </div>
-                </Col>
-              </Row>
-              <Row className="discount-separation-top">
-                <Col>
-                  <Image src={mon01} srcSet={`${mon02} 2x, ${mon03} 3x`} className="discount-monkey-img" fluid></Image>
-                </Col>
-              </Row>
-
-              <Row>
-                <Col xs={12} md={12} lg={12} className="discount-separation-top">
-                  <Image src={news_cats} className="img-fit" fluid></Image>
-
-                </Col>
-
-              </Row>
-
+      <div className="pt-10 pb-63">
+        <Container className="shadow px-0 ">
+          <Row className="px-0">
+            <Col xs={12} md={12} lg={12}>
+              <Image src={news_header} className="img-fit" fluid></Image>
 
             </Col>
+
           </Row>
 
+          <div className="discount-box ">
 
 
-        </div>
-        <Row className="p-0">
-          <Col xs={12} md={12} lg={12}>
-            <Image src={news_footer} className="img-fit" fluid></Image>
 
-          </Col>
+            <Row className="text-center">
+              <Col xs={12} md={12} lg={{span:7}}>
+                <Row>
+                  <Col>
+                    <div className="discount-title">Passport for a decentralized world</div>
+                  </Col>
+                </Row>
+                <Row className="text-left">
+                  <div className="discount-info">
+                    {new Date().toLocaleDateString()}
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    <Image src={comment_icon} className="discount-sub-icons" fluid></Image>
+                    &nbsp;
+                    01
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    <Image src={eye_icon} className="discount-sub-icons" fluid></Image>
+                    &nbsp;
+                    301
+                  </div>
 
-        </Row>
+                </Row>
+                <Row className="discount-separation-top">
+                  <Col>
+                    <Image src={art01} srcSet={`${art02} 2x, ${art03} 3x`} className="discount-article-img" fluid></Image>
 
-      </Container>
 
+                  </Col>
+
+                </Row>
+                <Row className="discount-separation-top">
+                  <Col className="discount-article">
+
+                    <p>
+                      The decentralized world of Web3 and open Metaverse is upsetting the relationship between platforms and their users. Platforms lose control of data and users are only recognized by their address on the local blockchain.
+
+                    </p>
+                    <p>
+                      The sovereign identity is then absent from the exchanges and it is the community which takes over, the community of a game or the Twitter community of the artist which proves the authenticity of the work linked to the NFT.
+
+                    </p>
+                    <p>
+
+                      But what happens if you leave your community, what happens if the tweet is deleted?
+
+                    </p>
+
+                    <p>
+                      For data it’s even worse, some try to put them in NFTs but not everything is moneyable and transferable. A certificate of professional experience or a loyalty card, whether attached to a user or to his avatar in a Metaverse, has nothing to do with a token.
+
+                    </p>
+
+                    <p>
+                      There is therefore a lack, an approach that allows us to reconstitute a support for what we need to keep with us in this new decentralized world.
+
+                    </p>
+
+                  </Col>
+
+                </Row>
+              </Col>
+              <Col xs={12} md={12} lg={{span:4,offset:1}}>
+                <Row className="text-center">
+                  <Col>
+                    <Row className="text-center">
+                      <Col>
+                        <div className="discount-perc">15%</div>
+                      </Col>
+                    </Row>
+                    <Row >
+                      <Col className="text-center">
+                        <div className="discount-perc-sub">Discount</div>
+                      </Col>
+                    </Row>
+
+                  </Col>
+                  <Col className="text-center">
+                    <div className="lg-only">
+                      <QRCode value={REACT_APP_QR_URL + "/discount-offer"+qr} size={128} />
+                    </div>
+                    <div className="sm-only">
+                      <Button className="btn-dark-submit " variant="primary" href={REACT_APP_WALLET_LINK + encodeURIComponent(REACT_APP_QR_URL + "/discount-offer" + qr)}>Click to add to wallet</Button>
+                    </div>
+                  </Col>
+                </Row>
+                <Row className="discount-separation-top">
+                  <Col>
+                    <Image src={mon01} srcSet={`${mon02} 2x, ${mon03} 3x`} className="discount-monkey-img" fluid></Image>
+                  </Col>
+                </Row>
+
+                <Row>
+                  <Col xs={12} md={12} lg={12} className="discount-separation-top">
+                    <Image src={news_cats} className="img-fit" fluid></Image>
+
+                  </Col>
+
+                </Row>
+
+
+              </Col>
+            </Row>
+
+
+
+          </div>
+          <Row className="p-0">
+            <Col xs={12} md={12} lg={12}>
+              <Image src={news_footer} className="img-fit" fluid></Image>
+
+            </Col>
+
+          </Row>
+
+        </Container>
+      </div>
     </main>
   );
 }
--- client/src/pages/EmployerHome/index.js
+++ client/src/pages/EmployerHome/index.js
@@ -20,7 +20,9 @@ import com_acc_3 from '../../assets/images/v1/pages/employer/ic_account_circle.s
 
 import { Link } from 'react-router-dom';
 import LoginForm from "../../components/LoginForm";
-import {ROUTES} from "../../constants";
+import { ROUTES } from '../../constants';
+
+import './styles.scss'
 
 const { REACT_APP_NODE_LOCAL, REACT_APP_QR_URL } = process.env;
 
@@ -31,63 +33,64 @@ function EmployerHome() {
 
   return (
     <main >
-      <Container fluid>
-        <Row className="">
-          <Col xs={{ offset: 1, span: 10 }} md={{ offset: 1, span: 10 }} lg={{ offset: 1, span: 10 }} className="turquoise-container content-padded-uni">
-            <Row>
-              <Col xs={12} md={12} lg={8} className="justify-diff">
+      <div className="pt-10 pb-63">
+        <Container fluid>
+          <Row className="">
+            <Col xs={{ offset: 1, span: 10 }} md={{ offset: 1, span: 10 }} lg={{ offset: 1, span: 10 }} className="turquoise-container content-padded-uni">
+              <Row>
+                <Col xs={12} md={12} lg={8} className="justify-diff">
 
-                <Image src={com_logo_1} srcSet={`${com_logo_2} 2x, ${com_logo_3} 3x`} className="uni-logo"  fluid></Image>
+                  <Image src={com_logo_1} srcSet={`${com_logo_2} 2x, ${com_logo_3} 3x`} className="uni-logo"  fluid></Image>
 
-              </Col>
-              <Col xs={12} md={12} lg={4} className="text-center">
-                <Image src={com_tools_1} srcSet={`${com_tools_2} 2x, ${com_tools_3} 3x`} className="uni-tools"  fluid></Image>
+                </Col>
+                <Col xs={12} md={12} lg={4} className="text-center">
+                  <Image src={com_tools_1} srcSet={`${com_tools_2} 2x, ${com_tools_3} 3x`} className="uni-tools"  fluid></Image>
 
-              </Col>
-            </Row>
-            <Row className="justify-diff lower">
+                </Col>
+              </Row>
+              <Row className="justify-diff lower">
 
-              <Col xs={12} md={12} lg={12}>
-                <Card >
-                  <Card.Body className="account-body text-center">
-                    <Row className="" >
-                      <Col className="text-center">
+                <Col xs={12} md={12} lg={12}>
+                  <Card >
+                    <Card.Body className="account-body text-center">
+                      <Row className="" >
+                        <Col className="text-center">
 
-                        <Image src={com_acc_1} srcSet={`${com_acc_2} 2x, ${com_acc_3} 3x`} className="icon-account" fluid></Image>
+                          <Image src={com_acc_1} srcSet={`${com_acc_2} 2x, ${com_acc_3} 3x`} className="icon-account" fluid></Image>
 
-                      </Col>
-
-
-                    </Row>
-                    <Row>
-                      <Col className="text-center">
-
-                        <div className="tag-h2-dark">My account</div>
-
-                      </Col>
+                        </Col>
 
 
-                    </Row>
-                    <Row>
-                      <Col className="text-center">
-                        <LoginForm next={ROUTES.COMPANY_ACCOUNT} />
+                      </Row>
+                      <Row>
+                        <Col className="text-center">
 
-                      </Col>
+                          <div className="tag-h2-dark">My account</div>
 
-                    </Row>
-
-                  </Card.Body>
-                </Card>
-
-              </Col>
-            </Row>
-          </Col>
-        </Row>
+                        </Col>
 
 
+                      </Row>
+                      <Row>
+                        <Col className="text-center">
+                          <LoginForm next={ROUTES.COMPANY_ACCOUNT} />
 
-      </Container>
+                        </Col>
 
+                      </Row>
+
+                    </Card.Body>
+                  </Card>
+
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+
+
+
+        </Container>
+      </div>
     </main>
   );
 }
